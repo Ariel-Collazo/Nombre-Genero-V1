ac60dff5c7b11b9fd54b54450c1fda5531724d48
Ariel <acollazode@gmail.com>
2025-12-09 22:11:55 -0300

    Correccion de los botones que aparecian en pantalla como los tres puntos y el boton del sobre que no cumplían ninguna utilidad. Fueron quitados para mayor comodidad. También se corrige que el teclado desaparezca de pantalla cuando se hace click en el boton de consulta, dejando libre la pantalla para ver el resultado.

--- app/src/main/java/com/example/nombre_genero_v1/FirstFragment.kt
+++ app/src/main/java/com/example/nombre_genero_v1/FirstFragment.kt
@@ -1,9 +1,12 @@
 package com.example.nombre_genero_v1
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import com.example.nombre_genero_v1.databinding.FragmentFirstBinding
 import com.example.nombre_genero_v1.api.RetrofitClient
@@ -25,12 +28,20 @@ class FirstFragment : Fragment() {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
     }
-
+    private fun hideKeyboard() {
+        val imm = requireActivity()
+            .getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        view?.let { v ->
+            imm.hideSoftInputFromWindow(v.windowToken, 0)
+            v.clearFocus()
+        }
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Binding directo
         binding.btnConsultar.setOnClickListener {
+            hideKeyboard();
             val nombre = binding.etNombre.text.toString().trim()
             if(nombre.isEmpty())
             {
